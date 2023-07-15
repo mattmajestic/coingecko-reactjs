c5ee6c5acee07e4c04c0c6302149fdaf70be9565
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-07-15 08:03:27 -0400

    update dependencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { Show } from "./Pages/Show";
