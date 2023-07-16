a02f6893c5243b07bb9d6664c63f8fc8cd6230e8
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-07-16 15:12:48 +0000

    metamask

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-export const Header = ({ back }) => {
+export function Header() {
+  const [metaMaskAddress, setMetaMaskAddress] = useState("");
+  const [isMetaMaskConnected, setIsMetaMaskConnected] = useState(false);
+
+  const connectToMetaMask = () => {
+    if (window.ethereum) {
+      window.ethereum
+        .request({ method: "eth_requestAccounts", params: [] })
+        .then((accounts) => {
+          const address = accounts[0];
+          setIsMetaMaskConnected(true);
+          setMetaMaskAddress(address);
+          console.log("MetaMask connected:", address);
+        })
+        .catch((error) => {
+          console.log("Error connecting to MetaMask:", error);
+        });
+    } else {
+      console.log("MetaMask extension not detected");
+    }
+  };
+
+  useEffect(() => {
+    if (metaMaskAddress) {
+      setIsMetaMaskConnected(true);
+    }
+  }, [metaMaskAddress]);
+
   return (
     <div className="header bg-gray-800 border-b py-4 border-gray-900">
       <div className="custom-maxWidth flex justify-between">
-        {back ? (
-          <Link to="/" className="flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 48 48"
-              width="24"
-            >
-              <path
-                fill="currentColor"
-                d="M28.05 36 16 23.95 28.05 11.9l2.15 2.15-9.9 9.9 9.9 9.9Z"
-              />
-            </svg>
-            Coiner
-          </Link>
-        ) : (
-          <Link to="/">Coiner</Link>
-        )}
+        <Link to="/" className="flex">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="24">
+            <path
+              fill="currentColor"
+              d="M28.05 36 16 23.95 28.05 11.9l2.15 2.15-9.9 9.9 9.9 9.9Z"
+            />
+          </svg>
+          Coiner
+        </Link>
 
         <ul className="flex justify-between gap-5">
           <li className="hover:text-gray-300 hover:underline">
@@ -30,12 +49,16 @@ export const Header = ({ back }) => {
             </Link>
           </li>
           <li>
-          <button className="button">
-              Connect to MetaMask
-            </button>
+            {isMetaMaskConnected ? (
+              <span>{metaMaskAddress}</span>
+            ) : (
+              <button className="button" onClick={connectToMetaMask}>
+                Connect MetaMask
+              </button>
+            )}
           </li>
         </ul>
       </div>
     </div>
   );
-};
+}
